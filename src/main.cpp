5d7f9949d9eee269f8d86c37b9ca800bc2662247
KeremTAN <keremtan.95@hotmail.com>
2023-01-18 17:17:07 +0300

    json directory is added to include dir

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include "logger.hpp"
 #include "json.h"
+#include "json-forwards.h"
+#include "json/json.h"
+#include "json/json_features.h"
+#include "json/allocator.h"
+#include "json/assertions.h"
+#include "json/config.h"
+#include "json/forwards.h"
+#include "json/reader.h"
+#include "json/value.h"
+#include "json/version.h"
+#include "json/writer.h"
 
 
 int main(int argc, char** argv){
